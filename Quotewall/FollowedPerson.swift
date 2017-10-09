84126e946c0b9f1853d7044db8821b164fb5201c
Bop <collin.cannavo@gmail.com>
2017-10-09 15:59:35 -0600

    Added notes on FollowedPerson model

--- Quotewall/FollowedPerson.swift
+++ Quotewall/FollowedPerson.swift
@@ -15,6 +15,7 @@ public class FollowedPerson{
     public static let phoneKey = "phone"
     public static let identifierKey = "identifier"
     public static let recordTypeKey = "FollowedPerson"
+    public static let userReferenceKey = "FollowedPersonReference"
     
     
     public let name: String
@@ -43,12 +44,33 @@ public class FollowedPerson{
         return record
     }
     
+    public var parentCKReference: CKReference?
     
+    public var ckReference: CKReference? {
+        guard let ckRecordID = ckRecordID else { return nil }
+        return CKReference(recordID: ckRecordID, action: .none)
+    }
+    
+    public init?(CKrecord: CKRecord) {
+        guard let name = CKrecord[FollowedPerson.nameKey] as? String,
+            let phone = CKrecord[FollowedPerson.phoneKey] as? String ,
+            let identifier = CKrecord[FollowedPerson.identifierKey] as? String
+            else { return nil}
+        
+        self.name = name
+        self.phone = phone
+        self.identifier = identifier
+
+        self.userCKReference = CKrecord[FollowedPerson.userReferenceKey] as? CKReference
+        
+        self.ckRecordID = CKrecord.recordID
+    }
 }
 
 /*
  
- FollowedPerson created by CNContact and references the User.
+ FollowedPerson created by CNContact and references the User.  The predicate looks for the phone number of the person after the User selects
+ The shared quote wall looks for ANY shared quotes and only shows the ones from the followedPersons
  
  
  
